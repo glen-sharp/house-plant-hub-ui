7966fb35064758859a9fc8aec4de76b12a85d57f
Glen Sharp <glen.sharp@ibm.com>
2025-04-30 21:23:19 +0100

    feat: added logout call

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -1,8 +1,10 @@
 import Cookies from 'js-cookie';
+import { UserLogout } from '../utils/serverRequests';
 
 export default function LogOut() {
 
     function LogOutUser() {
+        UserLogout()
         Cookies.remove('jwt');
         Cookies.remove('sessionid');
         window.location.replace('/login');
